c5cc5a81dd2149053ab1e74b598d1377a41acd4a
sonyvegass <amari.soniadz@gmail.com>
2023-12-14 20:19:17 +0100

    rectification orthographe dans le header

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
             <a href="profil.php">Profil</a>
             <a href="my-events.php">Mes fêtes</a>
             <a href="event-list.php">Liste des fêtes</a>
-            <a href="creation-event.php">Créer un fête</a>
+            <a href="creation-event.php">Créer une fête</a>
             <a href="logout.php">Déconnexion</a>
         <?php else : ?>
             <!-- Liens pour les visiteurs non connectés -->
